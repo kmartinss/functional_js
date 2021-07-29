c5632800976f026dd63181daf501185706ba85bf
KarineMartins <martinskazk@gmail.com>
2021-07-28 23:08:35 -0300

    promise #02

---
+++ functions/promise_2.js
@@ -0,0 +1,70 @@
+// setTimeout(function () {
+//     console.log('▒▒▒░░░░░░░░░░▄▐░░░░');
+
+//     setTimeout(function () {
+//         console.log('▒░░░░░░▄▄▄░░▄██▄░░░');
+
+//         setTimeout(function () {
+//             console.log('░░░░░░▐▀█▀▌░░░░▀█▄░');
+//             setTimeout(function () {
+//                 console.log('░░░░░░▐█▄█▌░░░░░░▀█▄');
+
+//                 setTimeout(function () {
+//                     console.log('░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀');
+
+//                     setTimeout(function () {
+//                         console.log('░░░░░▄▄▄██▀▀▀▀░░░░░');
+
+//                         setTimeout(function () {
+//                             console.log('░░░░█▀▄▄▄█░▀▀░░░░░░');
+
+//                             setTimeout(function () {
+//                                 console.log('░░░░▌░▄▄▄▐▌▀▀▀░░░░░');
+
+//                                 setTimeout(function () {
+//                                     console.log('░▄░▐░░░▄▄░█░▀▀░░░░░');
+//                                     setTimeout(function () {
+//                                         console.log('░▀█▌░░░▄░▀█▀░▀░░░░░');
+
+//                                         setTimeout(function () {
+//                                             console.log('░░░░░░░░▄▄▐▌▄▄░░░░░');
+
+//                                             setTimeout(function () {
+//                                                 console.log('░░░░░░░░▀███▀█░▄░░░');
+
+//                                                 setTimeout(function () {
+//                                                     console.log('░░░░░░░▐▌▀▄▀▄▀▐▄░░░');
+
+//                                                     setTimeout(function () {
+//                                                         console.log('░░░░░░░▐▀░░░░░░▐▌░░');
+
+//                                                         setTimeout(function () {
+//                                                             console.log('░░░░░░░█░░░░░░░░█░░ boa noite meu consagrado');
+//                                                         }, 300)
+//                                                     }, 300)
+//                                                 }, 300)
+//                                             }, 300)
+//                                         }, 300)
+//                                     }, 300)
+//                                 }, 300)
+//                             }, 300)
+//                         }, 300)
+//                     }, 300)
+//                 }, 300)
+//             }, 300)
+//         }, 300)
+//     }, 300)
+// }, 300)
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log('Executando promise...')
+            resolve()
+        }, tempo)
+    });
+}
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(esperarPor)
