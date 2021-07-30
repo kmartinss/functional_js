d460cdc63fd2a21bb2acc762b7e0e4d42da648e5
KarineMartins <martinskazk@gmail.com>
2021-07-29 22:59:20 -0300

    async/await

--- functions/promise_5.js
+++ functions/promise_5.js
@@ -2,7 +2,7 @@ function funcionarOuNao(valor, chanceErro) {
     return new Promise((resolve, reject) => {
 
         try {
-            con.log('temp');
+            con.log('temp'); //erro
             if (Math.random() < chanceErro) {
                 reject('Ocorreu um erro!');
             } else {
@@ -17,7 +17,7 @@ function funcionarOuNao(valor, chanceErro) {
 funcionarOuNao('testando...', 0.1)
     .then(v => `Valor: ${v}`)
     .then(
-        v => consol.log(v),
+        v => consol.log(v), //erro
         err => console.log(`Erro esp.: ${err}`)
     )
     .catch(err => console.log(`Erro geral: ${err}`))
