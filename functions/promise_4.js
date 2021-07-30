5644a4528f1fca685445d0b2be7fa76c940b3b55
KarineMartins <martinskazk@gmail.com>
2021-07-29 22:39:27 -0300

    promise #05 => tratando erros (try, catch)

--- functions/promise_4.js
+++ functions/promise_4.js
@@ -29,4 +29,4 @@ gerarVariosNumeros()
     .then(console.log)
     .then(() => {
         console.timeEnd('promise');
-    })
+    });
---
+++ functions/promise_5.js
@@ -0,0 +1,26 @@
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+
+        try {
+            con.log('temp');
+            if (Math.random() < chanceErro) {
+                reject('Ocorreu um erro!');
+            } else {
+                resolve(valor);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+funcionarOuNao('testando...', 0.1)
+    .then(v => `Valor: ${v}`)
+    .then(
+        v => consol.log(v),
+        err => console.log(`Erro esp.: ${err}`)
+    )
+    .catch(err => console.log(`Erro geral: ${err}`))
+    .then(() => console.log('Fim!'))
+
+//catch => trata o erro
